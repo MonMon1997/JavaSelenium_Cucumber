df7174b6ab435eb44b6958fdf95cac4e0ef8bc53
MonMon1997 <monmonmonmonmon11112@gmail.com>
2025-01-07 16:54:38 +0800

    2 Commit - renew the POM, and add the Hooks

--- src/test/java/StepDefinition/google.java
+++ src/test/java/StepDefinition/google.java
@@ -1,5 +1,9 @@
 package StepDefinition;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,6 +15,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.io.FileInputStream;
@@ -23,10 +28,11 @@ public class google {
     WebDriver driver = null;
     Logger logger;
 
-    @Given("User Nav to Google Main Page")
-    public void user_nav_to_google_main_page() throws IOException {
-
-        logger = LogManager.getLogger("");
+    @Before
+    public void browserSetup() throws InterruptedException, IOException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--desktop-window-1080p");
 
         Properties properties = new Properties();
         FileInputStream inputStream = new FileInputStream("C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_3\\src\\test\\java\\config\\config.properties");
@@ -35,12 +41,32 @@ public class google {
 
         if (browser.equalsIgnoreCase("Chrome")){
             System.setProperty("Chrome", "C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_3\\chromedriver.exe");
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("Edge")) {
             System.setProperty("Edge", "C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_3\\msedgedriver.exe");
             driver = new EdgeDriver();
         }
+    }
 
+//    @After
+//    public void teardown(){
+//        System.out.println("I am TearDown");
+//    }
+
+    @BeforeStep
+    public void before(){
+        System.out.println("----Before----");
+    }
+
+    @AfterStep
+    public void after(){
+        System.out.println("----After----");
+    }
+
+    @Given("User Nav to Google Main Page")
+    public void user_nav_to_google_main_page() throws IOException {
+
+        logger = LogManager.getLogger(google.class);
         driver.navigate().to("https://www.google.com/");
     }
 
@@ -65,25 +91,37 @@ public class google {
         logger.info("Test Finish");
     }
 
+    //Regression Test
     @Given("User Nav to Main Page")
     public void userNavToMainPage() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--desktop-window-1080p");
+
         System.setProperty("Chrome", "C:\\Program Files\\Intellij\\IdeaProjects\\Web_Automation_3\\chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get("https://www.google.com/");
+
+        logger = LogManager.getLogger(google.class);
     }
 
     @When("User Enter TextArea")
     public void userEnterTextArea() {
-        driver.findElement(By.name("q"));
+        google_POM text = new google_POM(driver);
+        text.clicktext();
+        logger.info("User Enter TextArea");
     }
 
     @And("User Enter TWICE")
     public void userEnterTWICE() {
-        driver.findElement(By.name("q")).sendKeys("TWICE");
+        google_POM text = new google_POM(driver);
+        text.searchtextname("TWICE");
+        logger.info("User Enter TWICE");
     }
 
     @Then("User Should See the Results")
     public void userShouldSeeTheResults() {
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        logger.info("User Should See the Results");
     }
 }
